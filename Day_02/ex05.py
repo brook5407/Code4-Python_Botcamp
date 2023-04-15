38df584e273422f7cdf4c7e708790c1806fc4942
Brook <chunyong96@gmail.com>
2023-04-15 13:17:41 +0800

    done day05 ex01 & ex02

--- Day_02/ex05.py
+++ Day_02/ex05.py
@@ -1,14 +1,11 @@
 def check_string(str):
-	if (str.find("The") != -1):
-		return "Yes!"
-	else:
-		return "No!"
+    return "Yes!" if str.startswith("The") else "NO"
 
 
 if __name__ == "__main__":
-	str1 = 'The'
-	str2 = 'Thumbs up'
-	str3 = 'Theatre can be boring'
-	print(check_string(str1))
-	print(check_string(str2))
-	print(check_string(str3))
+    str1 = 'The'
+    str2 = 'Thumbs up'
+    str3 = 'Theatre can be boring'
+    print(check_string(str1))
+    print(check_string(str2))
+    print(check_string(str3))
---
+++ Day_05/ex01.py
@@ -0,0 +1,25 @@
+import discord
+import random
+import requests
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+intents = discord.Intents.default()
+intents.message_content = True
+
+client = discord.Client(intents=intents)
+
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
+
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content == '$gif':
+
+client.run(os.getenv("TOKEN"))
